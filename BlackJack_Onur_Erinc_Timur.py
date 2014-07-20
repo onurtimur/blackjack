b413fe1734f1424288b2788f3bb9ac1acb713f03
onurtimur <onurerinctimur@gmail.com>
2014-07-20 23:40:21 +0300

    BlackJack_Onur_Erinc_Timur.py
    BlackJack game developed on Python.

--- BlackJack_Onur_Erinc_Timur.py
+++ BlackJack_Onur_Erinc_Timur.py
@@ -6,7 +6,7 @@ import sys
 
 """
 
-#House plays in this function.It changes the global variable named house_card_val which is for storing value of house's card.
+#loads new card deck
 def loadDeck():
 	global flag_house
 	global flag_player
@@ -27,6 +27,7 @@ def loadDeck():
 	player_card=""
 	house_card=""
 	cardo= {"AC":11,"2C":2,"3C":3,"4C":4,"5C":5,"6C":6,"7C":7,"8C":8,"9C":9,"10C":10,"QC":10,"JC":10,"KC":10,"AS":11,"2S":2,"3S":3,"4S":4,"5S":5,"6S":6,"7S":7,"8S":8,"9S":9,"10S":10,"QS":10,"JS":10,"KS":10,"AD":11,"2D":2,"3D":3,"4D":4,"5D":5,"6D":6,"7D":7,"8D":8,"9D":9,"10D":10,"QD":10,"JD":10,"KD":10,"AH":11,"2H":2,"3H":3,"4H":4,"5H":5,"6H":6,"7H":7,"8H":8,"9H":9,"10H":10,"QH":10,"JH":10,"KH":10}
+#House plays in this function.It changes the global variable named house_card_val which is for storing value of house's card.
 def housePlay(house_card_val_local,house_card_list):
 	
 	global house_card_val
